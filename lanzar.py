8fc21858614441957fb582763df6223c0450e4a7
sarudego <sruizdegopegui@gmail.com>
2017-05-29 22:09:42 +0200

    some updates

--- lanzar.py
+++ lanzar.py
@@ -7,6 +7,17 @@ import time
 opt = ['fb','av','pd','rp']
 dire = listdir('Pruebas')
 dire.sort()
+dire.pop(16) # fichero 20
+dire.pop(15) # fichero 19
+dire.pop(14) # fichero 18
+dire.pop(13) # fichero 17
+dire.pop(12) # fichero 16
+dire.pop(11) # fichero 15
+dire.pop(10) # fichero 14
+dire.pop(9) # fichero 13
+dire.pop(8) # fichero 12
+dire.pop(7) # fichero 11
+dire.pop(6) # fichero 10
 for i in opt:
     if (i == "fb"):
         print "Se va a ejecutar el algoritmo de \"Fuerza Bruta\" con ficheros de prueba"
@@ -22,7 +33,7 @@ for i in opt:
         os.system('python tsp.py ' + i + ' ' + 'Pruebas/' +j)
         tiempo = (time.time() - start_time)
         print "Ha costado " + str(tiempo) + " segundos"
+        print ""
     print ""
     print ""
-
 
--- p1.py
+++ p1.py
@@ -21,10 +21,10 @@ def fuerzaMatriz(matriz):
     for i in vertices:
         if i[0] == inicio:
             lst.append(i)
-    print lst
-    print "\n"
+    # print lst
+    # print "\n"
     caminos = []
-    print "Los caminos posibles son: "
+    # print "Los caminos posibles son: "
     for i in lst:
         suma = 0
         for j in range(len(i)):
@@ -32,11 +32,11 @@ def fuerzaMatriz(matriz):
                 suma += matriz[i[j]-1][i[j+1]-1]
             else:
                 suma += matriz[i[j]-1][inicio-1]
-        print repr(i) + " con distancia " + repr(suma)
+        # print repr(i) + " con distancia " + repr(suma)
         c = [suma, i]
         caminos.append(c)
     c = min(caminos)
-    print "\n"
+    # print "\n"
     print "El camino optimo en Fuerza Bruta es: " + repr(c[1])
     print "Con distancia " + repr(c[0])
 

6078e7dda98e4bfb184cfbbfb0a2a8cbc4d796f0
sarudego <sruizdegopegui@gmail.com>
2017-05-30 12:05:14 +0200

    write on file

--- lanzar.py
+++ lanzar.py
@@ -3,30 +3,35 @@
 import os
 from os import listdir
 import time
+"""
+En RyP, hasta 26x26
+En P.D, hasta 23x23
+"""
 
 opt = ['fb','av','pd','rp']
 dire = listdir('Pruebas')
 dire.sort()
-dire.pop(16) # fichero 20
-dire.pop(15) # fichero 19
-dire.pop(14) # fichero 18
-dire.pop(13) # fichero 17
-dire.pop(12) # fichero 16
-dire.pop(11) # fichero 15
-dire.pop(10) # fichero 14
-dire.pop(9) # fichero 13
-dire.pop(8) # fichero 12
-dire.pop(7) # fichero 11
-dire.pop(6) # fichero 10
+for i in range(60, 5, -1):
+    dire.pop(i)
+
 ficheros_grandes = listdir('Pruebas')
 ficheros_grandes.sort(reverse=True)
-ficheros_grandes.pop(16)
-ficheros_grandes.pop(15)
-ficheros_grandes.pop(14)
-ficheros_grandes.pop(13)
-ficheros_grandes.pop(12)
-ficheros_grandes.pop(11)
+for i in range(60, 54, -1):
+    ficheros_grandes.pop(i)
 ficheros_grandes.sort()
+ficheros_din = listdir('Pruebas')
+ficheros_din.sort()
+for i in range(60, 19, -1):
+    ficheros_din.pop(i)
+ficheros_din.sort(reverse=True)
+for i in range(19, 15, -1):
+    ficheros_din.pop(i)
+ficheros_din.sort()
+
+"""
+ejecucion hasta 10x10
+"""
+outfile = open('tiempos.txt', 'w') # Indicamos el valor 'w'.
 for i in opt:
     if (i == "fb"):
         print "Se va a ejecutar el algoritmo de \"Fuerza Bruta\" con ficheros de prueba"
@@ -36,31 +41,59 @@ for i in opt:
         print "Se va a ejecutar el algoritmo de \"Programacion Dinamica\" con ficheros de prueba"
     else:
         print "Se va a ejecutar el algoritmo de \"Ramificacion y Poda\" con ficheros de prueba"
+    outfile.write("Ejecucion de " + i + "\n")
     for j in dire:
         print "Se va a ejecutar el fichero... " + j
         start_time = time.time()
         os.system('python tsp.py ' + i + ' ' + 'Pruebas/' +j)
         tiempo = (time.time() - start_time)
+        outfile.write("El fichero " + j + " ha costado " + str(tiempo) + " segundos\n")
         print "Ha costado " + str(tiempo) + " segundos"
         print ""
+    outfile.write("\n")
     print ""
     print ""
 
 print "Ahora las matrices de 10x10 hasta 20x20... se toman su tiempo..."
 opt2 = ['av','pd','rp']
+outfile.write("\n\nAhora ficheros grandes...\n")
 for i in opt2:
+    outfile.write("\nEjecucion de " + i + "\n")
     if (i == "av"):
         print "Se va a ejecutar el algoritmo de \"Algoritmos Voraces\" con ficheros de prueba"
+        for j in ficheros_grandes:
+            print "Se va a ejecutar el fichero... " + j
+            start_time = time.time()
+            os.system('python tsp.py ' + i + ' ' + 'Pruebas/' +j)
+            tiempo = (time.time() - start_time)
+            outfile.write("El fichero " + j + " ha costado " + str(tiempo) + " segundos\n")
+            print "Ha costado " + str(tiempo) + " segundos"
+            print ""
     elif (i == "pd"):
         print "Se va a ejecutar el algoritmo de \"Programacion Dinamica\" con ficheros de prueba"
+        for j in ficheros_din:
+            print "Se va a ejecutar el fichero... " + j
+            start_time = time.time()
+            os.system('python tsp.py ' + i + ' ' + 'Pruebas/' +j)
+            tiempo = (time.time() - start_time)
+            outfile.write("El fichero " + j + " ha costado " + str(tiempo) + " segundos\n")
+            print "Ha costado " + str(tiempo) + " segundos"
+            print ""
     else:
         print "Se va a ejecutar el algoritmo de \"Ramificacion y Poda\" con ficheros de prueba"
-    for j in ficheros_grandes:
-        print "Se va a ejecutar el fichero... " + j
-        start_time = time.time()
-        os.system('python tsp.py ' + i + ' ' + 'Pruebas/' +j)
-        tiempo = (time.time() - start_time)
-        print "Ha costado " + str(tiempo) + " segundos"
-        print ""
+        for j in ficheros_grandes:
+            print "Se va a ejecutar el fichero... " + j
+            start_time = time.time()
+            os.system('python tsp.py ' + i + ' ' + 'Pruebas/' +j)
+            tiempo = (time.time() - start_time)
+            outfile.write("El fichero " + j + " ha costado " + str(tiempo) + " segundos\n")
+            print "Ha costado " + str(tiempo) + " segundos"
+            print ""
     print ""
     print ""
+
+outfile.close()
+# print dire
+# print ficheros_grandes
+# print ficheros_din
+
